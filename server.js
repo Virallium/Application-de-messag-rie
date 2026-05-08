e993978fc3d8656c8d91df218124610f428a066a
jotag243 <156473641+jotag243@users.noreply.github.com>
2026-05-08 15:41:27 +0100

    serveur

--- server.js
+++ server.js
@@ -1,57 +1,72 @@
-const express=require('express')
-const app=express()
-const http=require('http')
-const path=require('path')
-const { Server } = require('socket.io')
-const server=http.createServer(app)
-const io= new Server(server)
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 
-app.set('view engine','ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+const session = require("express-session");
 
-io.on('connection',(socket)=>{
-    console.log("Un utilisateur s'est conncecté ")
-    socket.on('chat',(data)=>{
-        io.emit('chat',data)
-    })
-})
-app.get('/register',(req,res)=>{
-    res.render('register',{
-        photo:'photo',
-        nom:'nom',
-        prenom:'prenom',
-        numero_tel:'numero_tel',
-        mdpass:'mdpass'
-    })
-})
-app.get('/login',(req,res)=>{
-    res.render('login',{
-        nom:'nom'
-    })
-})
-app.get('/',(req,res)=>{
-    res.render('index',{
-        message:'message',
-        data:'date'
-    })
-})
-app.get('archive/',(req,res)=>{
-    res.render('archive',{
-        nom:'',
-        msg:'message'
-    })
-})
+const io = new Server(server);
 
-app.get('/profil',(req,res)=>{
-    res.render('profil',{
-        photo:'photo',
-        nom:'nom',
-        prenom:'prenom',
-        descr:'descr'
-    })
-})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "mot-secret-pour-session",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false, // true si HTTPS
+      maxAge: 1000 * 60 * 60, // 1 heure
+    },
+  }),
+);
 
-server.listen(3000,()=>{
-    console.log('Le serveur est en marche')
-})
+io.on("connection", (socket) => {
+  console.log("Un utilisateur s'est conncecté ");
+  socket.on("chat", (data) => {
+    io.emit("chat", data);
+  });
+});
+app.get("/register", (req, res) => {
+  res.render("register", {
+    photo: "photo",
+    nom: "nom",
+    prenom: "prenom",
+    numero_tel: "numero_tel",
+    mdpass: "mdpass",
+  });
+});
+// app.get("/login", (req, res) => {
+//   res.render("login", {
+//     nom: "nom",
+//   });
+// });
+app.get("/", (req, res) => {
+  res.render("login", {
+    // message: "message",
+    // data: "date",
+  });
+});
+app.get("archive/", (req, res) => {
+  res.render("archive", {
+    nom: "",
+    msg: "message",
+  });
+});
+
+app.get("/profil", (req, res) => {
+  res.render("profil", {
+    photo: "photo",
+    nom: "nom",
+    prenom: "prenom",
+    descr: "descr",
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Le serveur est en marche");
+});
